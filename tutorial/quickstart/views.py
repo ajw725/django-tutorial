06c75f67c9cd72e68fe8e848d247073111af6d77
Andrew Weinstein <aweinstein7@gmail.com>
2021-05-21 11:30:19 -0600

    initial commit with quickstart project

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,22 @@
+# from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to view/edit users
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to view/edit groups
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
